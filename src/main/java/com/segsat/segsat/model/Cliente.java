ca2ca57ca73cae39bf558e619f9923bb67db20cb
joaomoraiss <joaovictorrmpework@outlook.com>
2024-12-20 08:12:53 -0300

    Adição lombok + Construtor sem campo telefone
    Lombok com proposito de reduzir boilerplate.
    
    Construtor sem telefone por não ser um campo obrigatório.

--- src/main/java/com/segsat/segsat/model/Cliente.java
+++ src/main/java/com/segsat/segsat/model/Cliente.java
@@ -2,9 +2,14 @@ package com.segsat.segsat.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "client")
+@NoArgsConstructor
+@Getter @AllArgsConstructor
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,4 +47,15 @@ public class Cliente {
     @Column(name = "estado")
     private String estado;
 
+    public Cliente(long id, String nome, String email, String cep, String endereco, String bairro,
+                   String cidade, String estado) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cep = cep;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
 }
