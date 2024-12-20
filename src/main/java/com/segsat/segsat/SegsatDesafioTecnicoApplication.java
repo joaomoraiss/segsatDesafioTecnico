48154d99798b780b3f6992269207e7e4fe73ad53
joaomoraiss <joaovictorrmpework@outlook.com>
2024-12-20 07:46:26 -0300

    Initial commit

---
+++ src/main/java/com/segsat/segsat/SegsatDesafioTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.segsat.segsat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegsatDesafioTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegsatDesafioTecnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/segsat/segsat/SegsatDesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segsat.segsat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegsatDesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
