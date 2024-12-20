ed0ccb80a79db1f5c430b7c4f024fc6336ea9596
joaomoraiss <joaovictorrmpework@outlook.com>
2024-12-20 08:48:43 -0300

    Camada service com Criação, busca, atualização e deleção de cliente

--- src/main/java/com/segsat/segsat/service/ClienteService.java
+++ src/main/java/com/segsat/segsat/service/ClienteService.java
@@ -5,15 +5,57 @@ import com.segsat.segsat.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    public void criarCliente(Cliente cliente){
-        if (clienteRepository.findByEmail(cliente.getEmail()) != cliente)
-            clienteRepository.save(cliente);
-        else throw new IllegalArgumentException("O email já está cadastrado.");
+    public void criarCliente(Cliente cliente) {
+        if (clienteRepository.existsByEmail(cliente.getEmail())){
+            throw new IllegalArgumentException("O email já está cadastrado.");
+        }
+        clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> getAllClientes(){
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> getClienteById(Long id) {
+        return Optional.ofNullable(clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado com o ID: " + id)));
+    }
+
+    public void deleteClienteById(long id){
+        if (!clienteRepository.existsById(id))
+            throw new RuntimeException("Deleção mal sucedida! Esse Cliente não foi encontrado" +
+                    " ou não existe.");
+
+        clienteRepository.deleteById(id);
+    }
+
+    public Cliente atualizarCliente(long id, Cliente novosDados) {
+        Cliente clienteExistente = clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado com o ID: " + id));
+
+        if (!clienteExistente.getEmail().equals(novosDados.getEmail()) &&
+                clienteRepository.existsByEmail(novosDados.getEmail())) {
+            throw new IllegalArgumentException("O e-mail já está em uso por outro cliente.");
+        }
+
+        clienteExistente.setNome(novosDados.getNome());
+        clienteExistente.setEmail(novosDados.getEmail());
+        clienteExistente.setTelefone(novosDados.getTelefone());
+        clienteExistente.setCep(novosDados.getCep());
+        clienteExistente.setEndereco(novosDados.getEndereco());
+        clienteExistente.setBairro(novosDados.getBairro());
+        clienteExistente.setCidade(novosDados.getCidade());
+        clienteExistente.setEstado(novosDados.getEstado());
+
+        return clienteRepository.save(clienteExistente);
     }
 
 }
